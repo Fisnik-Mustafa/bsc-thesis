469c3bfaa18806b7c924c727695f6d025ed54709
mustafaf <mustafaf@student.ethz.ch>
2022-03-26 11:00:18 +0100

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <h1>Zahlensysteme erkennen und verstehen.</h1>
-    <p>Test Test Test</p>
     <div class="tasks">
       <!-- DezimalSystem -->
       <router-link to="Dezimalsystem_1">
