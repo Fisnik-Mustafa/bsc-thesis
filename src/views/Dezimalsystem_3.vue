2bd48a7e75e80e4e5b8d5a8df1e9d5c717286817
mustafaf <mustafaf@student.ethz.ch>
2022-03-30 13:54:05 +0200

    Funktionalität fertig

--- src/views/Dezimalsystem_3.vue
+++ src/views/Dezimalsystem_3.vue
@@ -6,9 +6,13 @@
       Bestimme die beiden Summanden und addiere sie korrekt zusammen, indem du
       die Karten korrekt umtauschst
     </p>
-    <p>{{ summand1randomnumber }}</p>
-    <p>{{ summand2randomnumber }}</p>
-    <p>{{ summerandomnumber }}</p>
+
+    <Verifier
+      v-if="this.submitted"
+      :correctSolution="this.result"
+      :tip="'Achte darauf, dass du korrekt umtauscht!'"
+      @close-verifier="this.submitted = false"
+    />
     <hr style="color: black" />
 
     <h2>Summand 1</h2>
@@ -79,21 +83,49 @@
         <div v-for="index in summeeiner" :key="index" class="karte">1</div>
       </div>
     </div>
-    <br>
-    <div class="umtausch" v-if="addup">
-      <button>10000 <i class="arrow left"></i> 9*1000</button>
-      <button>1000 <i class="arrow left"></i> 9*100</button>
-      <button>100 <i class="arrow left"></i> 9*10</button>
-      <button>10 <i class="arrow left"></i> 9*1</button>
+    <br />
+    <div v-if="addup">
+      <button
+        class="umtausch"
+        v-if="morethan10tausender"
+        @click="changetausenderforzehntausender()"
+      >
+        10000 <i class="arrow left"></i> 9*1000
+      </button>
+      <button
+        class="umtausch"
+        v-if="morethan10hunderter"
+        @click="changehunderterfortausender()"
+      >
+        1000 <i class="arrow left"></i> 9*100
+      </button>
+      <button
+        class="umtausch"
+        v-if="morethan10zehner"
+        @click="changezehnerforhunderter()"
+      >
+        100 <i class="arrow left"></i> 9*10
+      </button>
+      <button
+        class="umtausch"
+        v-if="morethan10einer"
+        @click="changeeinerforzehner()"
+      >
+        10 <i class="arrow left"></i> 9*1
+      </button>
     </div>
+    <button @click="submit()" class="btn_submit" v-if="addup">Überprüfen</button>
+    <Nexttask />
   </div>
 </template>
 
 <script>
 import Backtohomepage from "@/components/Backtohomepage.vue";
+import Verifier from "@/components/Verifier.vue";
+import Nexttask from "@/components/Nexttask.vue";
 
 export default {
-  components: { Backtohomepage },
+  components: { Backtohomepage, Verifier, Nexttask },
   data() {
     return {
       summand2randomnumber: Math.floor(Math.random() * (9999 - 1 + 1)) + 1,
@@ -120,6 +152,12 @@ export default {
       resultzehner: 0,
       resulteiner: 0,
       addup: false,
+      morethan10tausender: false,
+      morethan10hunderter: false,
+      morethan10zehner: false,
+      morethan10einer: false,
+      result: false,
+      submitted: false,
     };
   },
   created: function () {
@@ -155,8 +193,71 @@ export default {
     this.resulteiner = this.summerandomnumber % 10;
   },
   methods: {
+    submit() {
+      this.checkResult();
+      this.submitted = true;
+    },
+    checkResult() {
+      if (
+        this.resultzehntausender == this.summezehntausender &&
+        this.resulttausender == this.summetausender &&
+        this.resulthunderter == this.summehunderter &&
+        this.resultzehner == this.summezehner &&
+        this.resulteiner == this.summeeiner
+      ) {
+        this.result = true;
+      }else{
+        this.result = false;
+      }
+    },
     add() {
       this.addup = true;
+      this.checkmorethan10();
+    },
+    checkmorethan10() {
+      if (this.summetausender > 9) {
+        this.morethan10tausender = true;
+      } else {
+        this.morethan10tausender = false;
+      }
+
+      if (this.summehunderter > 9) {
+        this.morethan10hunderter = true;
+      } else {
+        this.morethan10hunderter = false;
+      }
+
+      if (this.summezehner > 9) {
+        this.morethan10zehner = true;
+      } else {
+        this.morethan10zehner = false;
+      }
+
+      if (this.summeeiner > 9) {
+        this.morethan10einer = true;
+      } else {
+        this.morethan10einer = false;
+      }
+    },
+    changetausenderforzehntausender() {
+      this.summetausender = this.summetausender - 10;
+      this.summezehntausender = this.summezehntausender + 1;
+      this.checkmorethan10();
+    },
+    changehunderterfortausender() {
+      this.summehunderter = this.summehunderter - 10;
+      this.summetausender = this.summetausender + 1;
+      this.checkmorethan10();
+    },
+    changezehnerforhunderter() {
+      this.summezehner = this.summezehner - 10;
+      this.summehunderter = this.summehunderter + 1;
+      this.checkmorethan10();
+    },
+    changeeinerforzehner() {
+      this.summeeiner = this.summeeiner - 10;
+      this.summezehner = this.summezehner + 1;
+      this.checkmorethan10();
     },
   },
 };
@@ -180,5 +281,12 @@ export default {
 .left {
   transform: rotate(135deg);
   -webkit-transform: rotate(135deg);
+}
+.umtausch {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  width: 110px;
+  margin-right: 20px;
+  margin-left: 20px;
 }
 </style>
