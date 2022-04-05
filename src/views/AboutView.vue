26733b47ea007bfbcae48dc2b26a198feb326e07
mustafaf <mustafaf@student.ethz.ch>
2022-04-05 12:20:39 +0200

    About Page angepasst

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="about">
     <h1>Impressum</h1>
-    <p>Diese Lernplattform basiert auf das Lehrmittel 'Einfach Informatik 7-9'</p>
+    <Backtohomepage />
+    <p>
+      Diese Lernplattform basiert auf das Lehrmittel 'Einfach Informatik 7-9'
+    </p>
     <p>Entwickler: Fisnik Mustafa</p>
-    <router-link to="/">
+    <div>
+      Home icon made by
+      <a href="https://www.freepik.com" title="Freepik">Freepik</a> and
+      <a
+        href="https://www.flaticon.com/authors/vignesh-oviyan"
+        title="Vignesh Oviyan"
+      >
+        Vignesh Oviyan</a
+      >
+      respectively from <a href="https://www.flaticon.com/">www.flaticon.com</a>
+    </div>
+    <!--    <router-link to="/">
       <button class="back_to_homepage">
         Startseite
       </button>
-    </router-link>
+    </router-link> -->
   </div>
 </template>
 <script>
-export default {
-
-}
+import Backtohomepage from "@/components/Backtohomepage.vue";
+export default { components: { Backtohomepage } };
 </script>
 
 <style>
@@ -23,11 +36,12 @@ export default {
   right: 0px;
   margin: 20px;
   padding: 10px;
-  border:rgb(83, 81, 81);
+  border: rgb(83, 81, 81);
 }
 .back_to_homepage:hover {
   background-color: rgb(83, 81, 81);
   color: white;
-  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+  box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+    0 17px 50px 0 rgba(0, 0, 0, 0.19);
 }
 </style>
