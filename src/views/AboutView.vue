991d56d8c8b4c25af19386f78c1d572c64480b91
mustafaf <mustafaf@student.ethz.ch>
2022-04-30 16:46:56 +0200

    Impressum angepasst (Logos)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -46,6 +46,18 @@
     <br />
     <Backtohomepage />
     <Footer />
+    <div id="links">
+      <div id="logo-ethz">
+        <a href="https://ethz.ch" target="blank">
+        <img src="../assets/logos/ethz-logo.svg" draggable="false" style="height: 40px; margin-top: 10px; left: 50%;">
+        </a>
+      </div>
+      <div id="logo-abz">
+        <a href="https://abz.inf.ethz.ch/" target="blank">
+        <img src="../assets/logos/logo_abz_weiss.png" draggable="false" style="height: 60px; margin-top: 10px;"> 
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -68,5 +80,12 @@ export default { components: { Backtohomepage, Footer } };
   color: white;
   box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
     0 17px 50px 0 rgba(0, 0, 0, 0.19);
+}
+#links {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  
 }
 </style>
